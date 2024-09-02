99953749d2c1c0cec5c4e7fd20ac24b30d77a6b4
nilakantam <user27@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-02 11:53:11 +0000

    updating

---
+++ learning/Pranathi.c
@@ -0,0 +1,14 @@
+//single line comment
+/* multi line comment
+This is a hello world program
+Owner : Pranathi Date: 02-09-2024
+learnning history
+
+*/
+#include<stdio.h>
+int main()
+{
+	printf("My name is Pranathi\n");
+	printf("I am from Vijayawada/n");
+	return 0;
+}
